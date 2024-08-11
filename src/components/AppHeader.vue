9e537d0d540b2db8067645583578da8053ec2d94
george572 <khutiwork@gmail.com>
2024-08-11 11:35:53 +0400

    new menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,64 +1,62 @@
 <script setup>
-import { inject, ref, watch, computed } from 'vue';
-import { useStore } from '@/stores/index'
-import AppLanguageSwitcher from './AppLanguageSwitcher.vue'
-import BurgerMenuIcon from './BurgerMenuIcon.vue'
-import { useRouter, useRoute } from 'vue-router';
-
+import { inject, ref, watch, computed } from "vue";
+import { useStore } from "@/stores/index";
+import AppLanguageSwitcher from "./AppLanguageSwitcher.vue";
+import BurgerMenuIcon from "./BurgerMenuIcon.vue";
+import { useRouter, useRoute } from "vue-router";
 
 const store = useStore();
 const router = useRouter();
 const currentRoute = useRoute();
-const translations = inject('translations');
+const translations = inject("translations");
 const currentLanguage = ref(store.currentLanguage);
 const openNav = ref(false);
 
 const navigationLinks = [
   {
     title: translations[currentLanguage.value].header.navigation.clickCollect,
-    to : "/click-and-collect",
+    to: "/click-and-collect",
   },
   {
     title: translations[currentLanguage.value].header.navigation.menu,
-    to: "/menu"
+    to: "/menu",
   },
   {
-    title: 'logo',
+    title: "logo",
     href: "#header",
-    to: '/',
+    to: "/",
   },
   {
     title: translations[currentLanguage.value].header.navigation.about,
-    href : "#aboutus",
-    to: '/',
+    href: "#aboutus",
+    to: "/",
   },
   {
     title: translations[currentLanguage.value].header.navigation.contact,
-    href : "#contact",
-    to: '/',
+    href: "#contact",
+    to: "/",
   },
-]
+];
 
 const computedNavLinks = computed(() => {
-  return navigationLinks.filter((navLink) => navLink.title !== 'logo');
-})
+  return navigationLinks.filter((navLink) => navLink.title !== "logo");
+});
 
 const navigateTo = (routeEl) => {
   if (routeEl.href) {
-  router.push('/' + routeEl.href);
-  }
-  else {
+    router.push("/" + routeEl.href);
+  } else {
     router.push(routeEl.to);
   }
-  openNav.value = false
-}
+  openNav.value = false;
+};
 
 watch(openNav, () => {
   if (openNav.value) {
-    return document.body.style.overflow = 'hidden';
-  } 
-  document.body.style.overflow = 'visible';
-})
+    return (document.body.style.overflow = "hidden");
+  }
+  document.body.style.overflow = "visible";
+});
 </script>
 
 <template>
@@ -77,39 +75,36 @@ watch(openNav, () => {
         src="./../assets/images/logo.svg"
         alt=""
         class="w-[200px] lg:w-full"
-      >
-      <p
-        v-else
-        class="transform translate-y-0.5"
-      >
+      />
+      <p v-else class="transform translate-y-0.5">
         {{ navLink.title }}
       </p>
     </span>
-    <AppLanguageSwitcher class="absolute right-[45px] z-50 cursor-pointer lg:top-[30px] md:top-[32px] font-[Geomanist-Bold] text-ed-green  text-[14px] lg:text-[19px]" />
+    <AppLanguageSwitcher
+      class="absolute right-[45px] z-50 cursor-pointer lg:top-[30px] md:top-[32px] font-[Geomanist-Bold] text-ed-green text-[14px] lg:text-[19px]"
+    />
   </div>
 
   <!-- Mobile Nav -->
-  <div class="sticky top-0 z-50 flex items-center justify-between px-2 py-4 bg-white md:hidden">
-    <a
-      href="#header" 
-      @click="navigateTo('/')"
-    >
+  <div
+    class="sticky top-0 z-50 flex items-center justify-between px-2 py-4 bg-white md:hidden"
+  >
+    <span @click="router.push('/')">
       <img
         src="./../assets/images/logo.svg"
         alt=""
         class="w-[150px] sm:w-[200px] lg:w-full cursor-pointer"
-      >
-    </a>
-    <div class="flex items-center gap-1">
-      <AppLanguageSwitcher class="cursor-pointer lg:top-[31px] relative md:top-[27.1px] font-[Geomanist-Bold] text-ed-green text-[14px] lg:text-[19px]" />
-      <BurgerMenuIcon
-        :open-nav="openNav"
-        @click="openNav = !openNav"
       />
+    </span>
+    <div class="flex items-center gap-1">
+      <AppLanguageSwitcher
+        class="cursor-pointer lg:top-[31px] relative md:top-[27.1px] font-[Geomanist-Bold] text-ed-green text-[14px] lg:text-[19px]"
+      />
+      <BurgerMenuIcon :open-nav="openNav" @click="openNav = !openNav" />
     </div>
     <div
-      class="w-[0px] overflow-hidden h-[calc(100vh-50px)] bg-ed-green flex flex-col justify-center items-center gap-8 transition-all duration-300 absolute left-0 top-[56px] z-30" 
-      :class="{'w-[100%]' : openNav}"
+      class="w-[0px] overflow-hidden h-[calc(100vh-50px)] bg-ed-green flex flex-col justify-center items-center gap-8 transition-all duration-300 absolute left-0 top-[56px] z-30"
+      :class="{ 'w-[100%]': openNav }"
     >
       <span
         v-for="navLink in computedNavLinks"
@@ -117,9 +112,7 @@ watch(openNav, () => {
         class="font-[Geomanist-Bold] uppercase font-bol text-ed-yellow align-middle text-[30px] whitespace-nowrap active:text-white"
         @click="navigateTo(navLink)"
       >
-        <p
-          class="transform translate-y-0.5"
-        >
+        <p class="transform translate-y-0.5">
           {{ navLink.title }}
         </p>
       </span>
@@ -127,5 +120,4 @@ watch(openNav, () => {
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
--- src/views/MenuView.vue
+++ src/views/MenuView.vue
@@ -1,25 +1,60 @@
-<script setup lang='ts'>
-import { inject, watch, ref, onMounted } from 'vue';
-import { useStore } from '../stores';
-import { storeToRefs } from 'pinia';
+<script setup lang="ts">
+import { inject, watch, ref, onMounted } from "vue";
+import { useStore } from "../stores";
+import { storeToRefs } from "pinia";
+import AppButton from "../components/AppButton.vue";
 
-const translations = inject('translations');
+const translations = inject("translations");
 const store = useStore();
 const { currentLanguage } = storeToRefs(store);
-const pdfSrc = ref("")
+const pdfSrc = ref<null | string>(null);
 
-onMounted(() => {
-    currentLanguage.value === 'fr' ? pdfSrc.value = 'https://drive.google.com/file/d/1cwfneQJ9TZV_6sT0t6z05liyxcz8jZwi/preview':
-    pdfSrc.value = 'https://drive.google.com/file/d/1ZKD13gBo6EEYBte10w8bajQkh9t2tyu0/preview' 
-})
+onMounted(() => {});
 watch(currentLanguage, () => {
-    currentLanguage.value === 'fr' ? pdfSrc.value = 'https://drive.google.com/file/d/1cwfneQJ9TZV_6sT0t6z05liyxcz8jZwi/preview':
-    pdfSrc.value = 'https://drive.google.com/file/d/1ZKD13gBo6EEYBte10w8bajQkh9t2tyu0/preview'
-})
+  currentLanguage.value === "fr"
+    ? (pdfSrc.value =
+        "https://drive.google.com/file/d/1cwfneQJ9TZV_6sT0t6z05liyxcz8jZwi/preview")
+    : (pdfSrc.value =
+        "https://drive.google.com/file/d/1ZKD13gBo6EEYBte10w8bajQkh9t2tyu0/preview");
+});
+
+const showMenu = (rest) => {
+  if (rest === "1") {
+    currentLanguage.value === "fr"
+      ? (pdfSrc.value =
+          "https://drive.google.com/file/d/1cwfneQJ9TZV_6sT0t6z05liyxcz8jZwi/preview")
+      : (pdfSrc.value =
+          "https://drive.google.com/file/d/1ZKD13gBo6EEYBte10w8bajQkh9t2tyu0/preview");
+  } else {
+    currentLanguage.value === "fr"
+      ? (pdfSrc.value =
+          "https://drive.google.com/file/d/1rB5IOAqooItvU-Dgbpo6CcCw_68CGg9Z/preview")
+      : (pdfSrc.value =
+          "https://drive.google.com/file/d/1MbPqhhp_qQOLfOibo4vKSDmbmsi7KDj7/preview");
+  }
+};
 </script>
 
 <template>
+  <div
+    v-if="pdfSrc === null"
+    class="p-4 flex flex-col gap-6 items-center w-[50%] mx-auto mt-40"
+  >
+    <div
+      class="w-full text-center cursor-pointer border bg-ed-yellow text-ed-red p-4 uppercase rounded-[54px] hover:bg-ed-pink hover:text-ed-red font-[Geomanist-Bold] md:text-[18px] transition-all duration-175"
+      @click="showMenu('1')"
+    >
+      Menu for Elle&Dee’s - Notre-dame-de-Lorette
+    </div>
+    <div
+      class="w-full text-center cursor-pointer border bg-ed-yellow text-ed-red p-4 uppercase rounded-[54px] hover:bg-ed-pink hover:text-ed-red font-[Geomanist-Bold] md:text-[18px] transition-all duration-175"
+      @click="showMenu('2')"
+    >
+      Menu for Elle&Dee’s - Champs-Élysées
+    </div>
+  </div>
   <iframe
+    v-else
     id="menu"
     :src="pdfSrc"
     frameborder="0"
